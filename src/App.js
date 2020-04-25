2f951a05bd18df09ab7b56df0337ec05ef1ef958
Ebin Santy <47107330+ebinsanty@users.noreply.github.com>
2020-04-25 21:37:15 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -6,27 +6,25 @@ import getWeb3 from "./getWeb3";
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
-
+  state = {cost: 0, itemName: "exampleItem1", loaded:false};
   componentDidMount = async () => {
-    try {
-      // Get network provider and web3 instance.
-      const web3 = await getWeb3();
-
-      // Use web3 to get the user's accounts.
-      const accounts = await web3.eth.getAccounts();
-
-      // Get the contract instance.
-      const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
-      const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
-        deployedNetwork && deployedNetwork.address,
-      );
-
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+  try {
+  // Get network provider and web3 instance.
+  this.web3 = await getWeb3();
+  // Use web3 to get the user's accounts.
+  this.accounts = await this.web3.eth.getAccounts();
+  // Get the contract instance.
+  const networkId = await this.web3.eth.net.getId();
+  
+  this.itemManager = new this.web3.eth.Contract(
+  ItemManager.abi,
+  ItemManager.networks[networkId] && ItemManager.networks[networkId].address,
+  );
+  this.item = new this.web3.eth.Contract(
+  Item.abi,
+  Item.networks[networkId] && Item.networks[networkId].address,
+  );
+  this.setState({loaded:true});
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
